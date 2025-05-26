c552768807f6b83b7c23f49a0935a88834644d64
Shakin Ahammed Emon <165311768+shakin-emon01@users.noreply.github.com>
2025-05-27 01:08:34 +0600

    Create B - Basic Data Types.cpp

---
+++ B - Basic Data Types.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+     int I;
+     long long L;
+     char C;
+     float F;
+    double D;
+    cin>>I>>L>>C>>F>>D;
+    cout<<I<<endl<<L<<endl<<C<<endl
+    <<fixed<<setprecision(2)<<F<<endl<<fixed<<setprecision(1)<<D<<endl;
+    return 0;
+}
