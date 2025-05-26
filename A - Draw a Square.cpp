ce65347e155f0a1ae6f9fb0db8f8beecb848ea50
Shakin Ahammed Emon <165311768+shakin-emon01@users.noreply.github.com>
2025-05-27 00:51:53 +0600

    Create A - Draw a Square.cpp

---
+++ A - Draw a Square.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int t;
+	cin>>t;
+	while(t--) {
+		int l, r, u, d;
+		cin>>l>>r>>d>>u;
+	if	(l==r && l==u && l==d) cout<<"Yes\n";
+	else cout<<"No\n";
+	}
+	return 0;
+}
