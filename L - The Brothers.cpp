bafed70dfe42332b2ed771ead93d0e9b5d425d5b
Shakin Ahammed Emon <165311768+shakin-emon01@users.noreply.github.com>
2025-05-27 01:11:15 +0600

    Create L - The Brothers.cpp

---
+++ L - The Brothers.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   string x1, y1;
+   string x2, y2;
+    cin>>x1>>y1>>x2>>y2;
+    if(y1==y2)
+    cout<<"ARE Brothers"<<endl;
+    else cout<<"NOT\n";
+    return 0;
+}
