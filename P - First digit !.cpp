89c608d280fe3c73b1789df801ad720c5c77c249
Shakin Ahammed Emon <165311768+shakin-emon01@users.noreply.github.com>
2025-05-27 01:16:28 +0600

    Create P - First digit !.cpp

---
+++ P - First digit !.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    int f = x/1000;
+    if(f%2==0) cout<<"EVEN\n";
+    else cout<<"ODD\n";
+    return 0;
+    }
