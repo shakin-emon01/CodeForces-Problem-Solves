182d41d6dec023147977ebc0b24e666a73034463
Shakin Ahammed Emon <165311768+shakin-emon01@users.noreply.github.com>
2025-05-27 01:14:44 +0600

    Create N - Char.cpp

---
+++ N - Char.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char x;
+    cin>>x;
+    if(x>='a' && x<='z'){
+        x=x-32;
+    }
+    else if(x>='A' && x<='Z'){
+        x=x+32;
+    }
+cout<<x<<endl;
+    return 0;
+}
