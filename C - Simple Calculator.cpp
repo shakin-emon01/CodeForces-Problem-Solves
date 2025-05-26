bdc97c0eb843f9e230f7a398c242e5c24ba6a1db
Shakin Ahammed Emon <165311768+shakin-emon01@users.noreply.github.com>
2025-05-27 01:10:42 +0600

    Create C - Simple Calculator.cpp

---
+++ C - Simple Calculator.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   long long int x, y;
+    cin>>x>>y;
+    long long int mul (x*y);
+    cout<<x<<" + "<<y<<" = "<<x+y<<endl;
+    cout<<x<<" * "<<y<<" = "<<mul<<endl;
+    cout<<x<<" - "<<y<<" = "<<x-y<<endl;
+    return 0;
+}
