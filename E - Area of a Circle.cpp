56f41a4999152c7e6ccb42e2bf65d9a5a9c61ef5
Shakin Ahammed Emon <165311768+shakin-emon01@users.noreply.github.com>
2025-05-27 01:12:36 +0600

    Create E - Area of a Circle.cpp

---
+++ E - Area of a Circle.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double a;
+    cin>>a;
+    double PI=3.141592653;
+    double Area = PI*(a*a);
+     cout << fixed << setprecision(9)<<Area<<endl;
+     return 0;
+}
