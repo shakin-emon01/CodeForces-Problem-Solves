94a947e0528f8c4694d3194ec8ed57af7f206acc
Shakin Ahammed Emon <165311768+shakin-emon01@users.noreply.github.com>
2025-05-27 01:14:24 +0600

    Create J - Multiples.cpp

---
+++ J - Multiples.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h> 
+using namespace std;
+int main() {
+    long long x, y;
+    cin>>x>>y;
+    if(x%y==0 || y%x==0) cout<<"Multiples\n";
+    else cout<< "No Multiples\n";
+    return 0; 
+} 
